29b3f86e0ce27df6e7a707dced5e9c3b5408b75b
Melisa96 <yerodriguez@uc.cl>
2022-05-07 13:30:48 -0400

    add delete documents with firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore, collection, onSnapshot,
+  addDoc, deleteDoc, doc, orderBy, query, serverTimestamp
+} from 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5o_md5Op4pE8nMWVfpZoccwhWT7S7p-E",
+  authDomain: "webg10.firebaseapp.com",
+  projectId: "webg10",
+  storageBucket: "webg10.appspot.com",
+  messagingSenderId: "1048362480638",
+  appId: "1:1048362480638:web:dbc73d7b4714209176c6af",
+  measurementId: "G-DMFEH25Z0F"
+};
+
+// init firebase app
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+
+// collection ref
+const colRef = collection(db, 'twits')
+
+// queries
+const q = query(colRef, orderBy('createdAt'))
+
+// get collection data
+onSnapshot(colRef, (snapshot) => {
+  let twits = []
+  snapshot.docs.forEach(doc => {
+    twits.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(twits)
+})
+
+
+// adding docs
+const addTwitForm = document.querySelector('.add') 
+addTwitForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addTwitForm.title.value,
+    createdAt: serverTimestamp()
+  })
+  .then(() => {
+    addTwitForm.reset()
+  })
+})
+
+// deleting docs
+const deleteTwitForm = document.querySelector('.delete')
+deleteTwitForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'twits', deleteTwitForm.id.value)
+
+  deleteDoc(docRef)
+    .then(() => {
+      deleteTwitForm.reset()
+    })
+})
+
+
+
+
+
 
 async function fetchBirds() {
   const res = await fetch('https://aves.ninjas.cl/api/birds');
